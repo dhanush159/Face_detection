3fa93c1de2027b1849c39736f0ff1184001e18d6
dhanush159 <98449608+dhanush159@users.noreply.github.com>
2023-01-04 22:48:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Disclaimer: This detection for video will work only for inbuilt webcam
 #              and not for external webcam and the detection for image will
 #              work only for images with single face(Smile only, face detection works for multiple faces)
+# Please note both xml files must be downloaded along with the code
 import cv2 as cv
 
 img = cv.imread("test.jpg")
